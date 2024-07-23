22240f88626d214c5c1a118833093e2b5d2fd22d
mstout60 <mstout@eprospector.com>
2024-07-23 12:19:06 -0500

    Implement tiptap editor

--- src/app/(main)/workspace/[workspaceId]/channels/[channelId]/page.tsx
+++ src/app/(main)/workspace/[workspaceId]/channels/[channelId]/page.tsx
@@ -1,13 +1,14 @@
 import { getUserData } from "@/actions/get-user-data";
 import { getUserWorkspaceChannels } from "@/actions/get-user-workspace-channels";
 import { getCurrentWorkspaceData, getUserWorkspaceData } from "@/actions/workspaces";
+import ChatHeader from "@/components/chat-header";
 import InfoSection from "@/components/info-section"
 import Sidebar from "@/components/sidebar"
+import TextEditor from "@/components/text-editor";
 import Typography from "@/components/ui/typography"
 
 import { Workspace as UserWorkspace } from "@/types/app";
 import { redirect } from "next/navigation";
-
 
 const ChannelIdPage = async (
   { params: { workspaceId, channelId },
@@ -21,29 +22,41 @@ const ChannelIdPage = async (
 
   if (!userData) return redirect('/auth');
 
-
   const [userWorkspacesData] = await getUserWorkspaceData(userData.workspaces!);
   const [currentWorkspaceData] = await getCurrentWorkspaceData(workspaceId);
   const userWorkspaceChannels = await getUserWorkspaceChannels(currentWorkspaceData.id, userData.id);
 
+  const currentChannelData = userWorkspaceChannels.find(channel => channel.id === channelId);
+
+  if (!currentChannelData) return redirect('/');
+
   return (
     <div className="hidden md:block">
-      <Sidebar
-        currentWorkspaceData={currentWorkspaceData}
-        userData={userData}
-        userWorkspacesData={userWorkspacesData as UserWorkspace[]}
-      />
-      <InfoSection
-        currentWorkspaceData={currentWorkspaceData}
-        userData={userData}
-        userWorkspaceChannels={userWorkspaceChannels}
-        currentChannelId={channelId}
-      />
-      <div className="p-2">
-        <Typography text="Channel ID Page" variant="p" />
+      <div className="h-[calc(100vh-256px)] overflow-y-auto [&::-webkit-scrollbar-thumb]:rounded-[6px]  [&::-webkit-scrollbar-thumb]:bg-foreground/60  [&::-webkit-scrollbar-track]:bg-none  [&::-webkit-scrollbar]:w-2 ">
+        <Sidebar
+          currentWorkspaceData={currentWorkspaceData}
+          userData={userData}
+          userWorkspacesData={userWorkspacesData as UserWorkspace[]}
+        />
+        <InfoSection
+          currentWorkspaceData={currentWorkspaceData}
+          userData={userData}
+          userWorkspaceChannels={userWorkspaceChannels}
+          currentChannelId={channelId}
+        />
+        <div className="p-4 relative w-full overflow-hidden">
+          <ChatHeader title={currentChannelData.name} />
+
+          <div className="mt-10">
+            <Typography text="Chat Content" variant="h4" />
+          </div>
+        </div>
+      </div>
+      <div className="mt-10">
+        <TextEditor />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChannelIdPage
+export default ChannelIdPage;
---
+++ src/components/text-editor.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { FiPlus } from "react-icons/fi";
+import { Button } from "@/components/ui/button";
+import { Send } from "lucide-react";
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import PlaceHolder from '@tiptap/extension-placeholder';
+import MenuBar from "@/components/menu-bar";
+
+
+const TextEditor = () => {
+    const editor = useEditor({
+        extensions: [StarterKit,
+            PlaceHolder.configure({
+                placeholder: `Message #${'channelName' ?? 'USERNAME'}`,
+            }),
+        ],
+    });
+
+    return (
+        <div className="p-1 border dark:border-zinc-500 border-neutral-700 rounded-md relative  overflow-hidden">
+            <div className="sticky top-0 z-10">
+                {/* menu bar */}
+                {editor && <MenuBar editor={editor}/>}
+            </div>
+            <div className="h-[150px] pt-11 flex w-full grow-1">
+                {/* Editor Content */}
+                <EditorContent
+                className="prose w-full h-full dark:text-white leading-[1.15px] overflow-y-hidden whitespace-pre-wrap"
+                 editor={editor} />
+            </div>
+            <div className="absolute top-3 z-10 right-3 bg-black dark:bg-white cursor-pointer transition-all duration-500 hover:scale-110 text-white grid place-content-center rounded-full w-6 h-6">
+                {/* Icon */}
+                <FiPlus size={28} className="dark:text-black" />
+            </div>
+            <Button size="sm" className="absolute bottom-1 right-1">
+                <Send />
+            </Button>
+        </div>
+    );
+};
+
+export default TextEditor;
